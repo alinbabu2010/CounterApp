e08f8ba93a2315e9dee4a86ae77be0b315ee0ba7
Alin Babu <alinbabu2010@gmail.com>
2023-04-24 12:04:55 +0530

    Add bloc provider to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:counter_app_bloc/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'bloc/bloc_impots.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,11 +12,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
