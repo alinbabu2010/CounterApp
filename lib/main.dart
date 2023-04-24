6d69ddf7f717e0a80e11c5b4956b1448555270ea
Alin Babu <alinbabu2010@gmail.com>
2023-04-24 14:30:34 +0530

    Add hydrated_bloc initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
-import 'package:counter_app_bloc/bloc/app_bloc_observer.dart';
 import 'package:counter_app_bloc/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'bloc/bloc_impots.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory());
+  HydratedBloc.storage = storage;
   Bloc.observer = AppBlocObserver();
   runApp(const MyApp());
 }
