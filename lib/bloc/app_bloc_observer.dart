afa6fa6067d380e132124697496fd48a2058ff16
Alin Babu <alinbabu2010@gmail.com>
2023-04-24 12:36:57 +0530

    Add custom bloc observer

---
+++ lib/bloc/app_bloc_observer.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:bloc/bloc.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    log("${bloc.runtimeType} $event");
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    log(error.toString());
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log("$change");
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    log("$transition");
+  }
+}
